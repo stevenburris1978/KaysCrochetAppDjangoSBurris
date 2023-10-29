a90f3a2c9aadd9c9df0283728127f8c8284a8423
stevenburris1978 <s_burris@mail.fhsu.edu>
2023-10-29 10:46:46 -0500

    modified admin and added list-filter

--- kayscrochetapp/models.py
+++ kayscrochetapp/models.py
@@ -1,4 +1,7 @@
 from django.db import models
+from django.contrib import admin
+import datetime
+from django.utils import timezone
 
 
 class Question(models.Model):
@@ -8,6 +11,11 @@ class Question(models.Model):
     def __str__(self):
         return self.question_text
 
+    @admin.display(
+        boolean=True,
+        ordering="pub_date",
+        description="Published recently?",
+    )
     def was_published_recently(self):
         return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
 
@@ -19,4 +27,3 @@ class Choice(models.Model):
 
     def __str__(self):
         return self.choice_text
-
