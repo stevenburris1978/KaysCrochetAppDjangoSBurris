f08756eb140e69b6bcfe1cdd909bb1d1cdccbc2f
stevenburris1978 <s_burris@mail.fhsu.edu>
2023-11-25 14:56:07 -0600

    Your commit message here

--- KaysCrochet/settings.py
+++ KaysCrochet/settings.py
@@ -32,8 +32,8 @@ SECRET_KEY = os.environ.get(
 IS_HEROKU_APP = "DYNO" in os.environ and not "CI" in os.environ
 
 # SECURITY WARNING: don't run with debug turned on in production!
-if not IS_HEROKU_APP:
-    DEBUG = True
+
+DEBUG = True
 
 if IS_HEROKU_APP:
     ALLOWED_HOSTS = ["*", "kayscrochet.us", "www.kayscrochet.us"]
