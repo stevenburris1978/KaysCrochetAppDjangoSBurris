edf8cc84968639229807c3c07eea8458cdf9f6f4
stevenburris1978 <s_burris@mail.fhsu.edu>
2023-11-25 00:03:29 -0600

    changed settings.py

--- KaysCrochet/settings.py
+++ KaysCrochet/settings.py
@@ -14,6 +14,7 @@ import dj_database_url
 import os
 from pathlib import Path
 
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -147,7 +148,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR / "staticfiles"
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
