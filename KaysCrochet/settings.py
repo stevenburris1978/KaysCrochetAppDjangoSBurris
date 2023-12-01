5e1df3648ef1a4d6427e10872c7d129e2a9e2e66
stevenburris1978 <s_burris@mail.fhsu.edu>
2023-12-01 15:28:15 -0600

    Your commit message here

--- KaysCrochet/settings.py
+++ KaysCrochet/settings.py
@@ -153,7 +153,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
-AWS_STORAGE_BUCKET_NAME = 'kayscrochetbucket'
+AWS_STORAGE_BUCKET_NAME = 'kayscrochetappbucket'
 AWS_S3_REGION_NAME = 'us-east-2'
 AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
 AWS_QUERYSTRING_AUTH = False
@@ -162,7 +162,7 @@ AWS_S3_FILE_OVERWRITE = False
 AWS_S3_SIGNATURE_VERSION = 's3v4'
 
 MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/'
-DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+DEFAULT_FILE_STORAGE = 'KaysCrochet.storage_backends.MediaStorage'
 
 AUTH_USER_MODEL = 'auth.User'
 
